1854d244afc04bbee5dbaae7abb48b0d64a50138
Joy Mathews <joymathe@gmail.com>
2024-12-01 10:33:53 +0000

    Fix typos in salary test scenarios and update test names for clarity

--- hra_tax_exemption/tests/salary_tests.cpp
+++ hra_tax_exemption/tests/salary_tests.cpp
@@ -6,12 +6,12 @@ namespace HRAExemptionTests {
 
 	//Feature: Salary
 
-	// Scenario: Salary percentage Fourty Percentage
+	// Scenario: Salary percentage Forty Percentage
 	//
 	// Given monthly basic salary and HRA,
 	// When a Salary class object is created with this information
 	// Then 40% of basic salary per year must be returned
-	TEST(Salary, FourtyPercentage) {
+	TEST(Salary, FortyPercentage) {
 		Salary salary(100, 40);
 		EXPECT_EQ(salary.getFortyPercentOfBasicSalary(), (100 * 12) * 0.4);
 	}
@@ -36,12 +36,12 @@ namespace HRAExemptionTests {
 		EXPECT_EQ(salary.getTenPercentOfBasicSalary(), (100 * 12) * 0.1);
 	}
 
-	// Scenario: Retreive HRA
+	// Scenario: Retrieve HRA
 	//
 	// Given monthly basic salary and HRA,
 	// When a Salary class object is created with this information
-	// Then hra per yesr must be returned
-	TEST(Salary, RetreiveHRA) {
+	// Then hra per year must be returned
+	TEST(Salary, RetrieveHRA) {
 		Salary salary(100, 40);
 		EXPECT_EQ(salary.getHRA(), 40 * 12);
 	}
