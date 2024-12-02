05817126b1471a991a41960f4bd79a0c7d6c1761
Joy Mathews <joymathe@gmail.com>
2024-12-02 03:29:49 +0000

    Refactor ExemptionCalculator: update implementation and add comprehensive tests

--- hra_tax_exemption/include/excemption_calculator/exemption_calculator.h
+++
@@ -1,9 +0,0 @@
-#ifndef EXEMPTION_CALCULATOR_H
-#define EXEMPTION_CALCULATOR_H
-
-class ExemptionCalculator {
-public:
-    double calculateActualRentMinusTenPercentageBasicSalary();
-};
-
-#endif // EXEMPTION_CALCULATOR_H
---
+++ hra_tax_exemption/include/exemption_calculator.h
@@ -0,0 +1,23 @@
+#ifndef EXEMPTION_CALCULATOR_H
+#define EXEMPTION_CALCULATOR_H
+
+#include "salary.h"
+#include "rent.h"
+
+namespace HRAExemption
+{
+	class ExemptionCalculator
+	{
+	private:
+		Salary salaryDetails;
+		Rent rentDetails;
+		bool metroCity;
+		double calculateActualRentMinusTenPercentageBasicSalary();
+		double calculateBasicSalaryExemption();
+	public:
+		ExemptionCalculator(Salary salary, Rent rent, bool livingInMetroCity);
+		double calculateExemption();
+	};
+}
+
+#endif // EXEMPTION_CALCULATOR_H
